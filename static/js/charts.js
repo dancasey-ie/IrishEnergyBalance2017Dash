525f0e64603bec223e430c9400d99067fe68a6e1
Daniel Casey <dcasey720@gmail.com>
2019-01-07 15:22:28 +0000

    Added byfuel pie chart.

--- static/js/charts.js
+++ static/js/charts.js
@@ -7,12 +7,13 @@ queue()
 function makeGraphs(error, energyData) {
     var ndx = crossfilter(energyData);
     show_consumptionByConsumer_barchart(ndx);
+    show_fuelType_pie(ndx);
     dc.renderAll();
 }
 
 // Energy Consumption (ktoe) per consumer, bar chart
 function show_consumptionByConsumer_barchart(ndx) {
-    var consumer_dim = ndx.dimension(dc.pluck('group'))
+    var consumer_dim = ndx.dimension(dc.pluck('subgroup'))
     var total_perConsumer = consumer_dim.group().reduceSum(dc.pluck('value'));
     dc.barChart("#consumptionByConsumer_barchart")
         .width(600)
@@ -27,4 +28,15 @@ function show_consumptionByConsumer_barchart(ndx) {
         .yAxis().ticks(4);
 }
 
+function show_fuelType_pie(ndx) {
+    var fuelType_dim = ndx.dimension(dc.pluck('fuelType'))
+    var all = fuelType_dim.groupAll().reduceSum(dc.pluck('value'));
+    var total_perFuelType = fuelType_dim.group().reduceSum(dc.pluck('value'));
+    dc.pieChart('#fuelType_pie')
+        .dimension(fuelType_dim)
+        .group(total_perFuelType)
+        .title(function (d) {
+            return d.key + ':\n' + Math.round(d.value / all.value() * 100) + '%\n' + Math.round(d.value) + 'toe';
+        });
+}
 
